f4ab7c6a384a8579b083fd99900b32419d4acd98
Daney <daneydeveloper@gmail.com>
2019-08-18 10:33:08 -0300

    Events Listener

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -8,6 +8,7 @@ class CalcController {
         this._currentDate
         this._locale = 'pt-BR'
         this.initialize()
+        this.initButtonsEvents()
     }
 
     initialize(){
@@ -15,17 +16,37 @@ class CalcController {
         this.setDisplayDateTime()
 
         setInterval(()=>{
-
             this.setDisplayDateTime()    
         
         }, 1000)
     }
 
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        })
+    }
+
     initButtonsEvents(){
+
         let buttons = document.querySelectorAll('#buttons > g, #parts > g')
+        
+        buttons.forEach((btn, index) => {
+
+            this.addEventListenerAll(btn, 'click drag', e => { 
+                console.log(btn.className.baseVal.replace("btn-",""))
+            })
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+                btn.style.cursor = "pointer";
+            })
+        })
+
     }
 
     setDisplayDateTime(){
+
         this.displayDate = this.currentDate.toLocaleDateString(this.locale)
         this.displayTime = this.currentDate.toLocaleTimeString(this.locale)
     }
